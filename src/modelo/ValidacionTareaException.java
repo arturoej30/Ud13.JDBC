4bd024134e3230438f33a04e03318a5677d83e10
Arturo Espinós Jiménez <249415754+arturoej30@users.noreply.github.com>
2026-06-02 11:45:27 +0200

    Delete src directory

--- src/modelo/ValidacionTareaException.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-public class ValidacionTareaException extends Exception {
-    // Excepción personalizada para validaciones
-        public ValidacionTareaException(String mensaje) {
-            super(mensaje);
-        }
-}
